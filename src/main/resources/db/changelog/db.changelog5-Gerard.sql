161d7d7d1a693c8c5229c905a29ae6f1e30460b7
Violetta <fif13@mail.ru>
2021-01-17 17:02:54 +0100

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	src/main/resources/db/changelog/db.changelog-3.sql

--- src/main/resources/db/changelog/db.changelog5-Gerard.sql
+++ src/main/resources/db/changelog/db.changelog5-Gerard.sql
@@ -1,3 +1,32 @@
+--
+create or replace function
+    best_player(player_id bigint, first_name text, last_name text, player_position text, points double precision)
+    returns double precision as
+$$
+begin
+    select distinct on (p.player_position)
+    (p.first_name || p.last_name) as "full_name",
+    a.player_id, p.player_position, a.points
+    from averages as a
+    join players as p on a.player_id = p.id
+    order by p.player_position, a.points desc;
+    return points;
+    end;
+$$ language plpgsql;
+--
+create or replace function
+    top_cities(team_id bigint, city text, points integer)
+    returns integer as
+$$
+begin
+    select s.team_id, t.city, sum(s.points)
+    from stats as s join teams t on s.team_id = t.id
+    order by s.points desc
+    limit 10;
+    return points;
+end;
+$$ language plpgsql;
+---
 create or replace function
     center_player(minutes text, Player_ID bigint, first_name text, last_name text, pos text)
     returns text as
@@ -18,6 +47,19 @@ end;
 $$ language plpgsql;
 
 -- Correlation between height of player and how many 3-pointers he made V
+create or replace function
+    corr_height_player(player_id bigint, height_inches integer, three_pointers_made double precision)
+    returns double precision as $$
+begin
+    select
+        a.player_id,
+        p.height_inches,
+        a.three_pointers_made
+    from averages as a join players as p on a.player_id = p.id
+    group by p.height_inches;
+end;
+$$ language plpgsql;
+
 -- Sorting by division show player name  with more than 15 points and 7  average assist G
 create or replace function
     sort_by_division(division text, points double precision, assist double precision)
@@ -36,6 +78,18 @@ begin
 end;
 $$ language plpgsql;
 -- Find a games in which the number of turnovers of the winner team is more than the turnovers of  looser team.  V
+--this is INCORRECT!
+create or replace function
+    turnover_stat(id bigint, turnovers double precision, name text, points double precision)
+    returns double precision as $$
+begin
+    select
+        g.id, s.turnovers, t.name, s.points
+    from games as g join stats s on g.id = s.game_id
+    join teams t on s.team_id = t.id;
+end;
+$$ language plpgsql;
+
 -- Show the games and players in which players has at least 10 assists, 10 points and has won a match (player were in winning team)G
 create or replace function
     show_specific_players(points double precision, assist double precision)
