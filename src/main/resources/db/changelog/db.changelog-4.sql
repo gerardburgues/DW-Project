965eaa626fedadf6372d69bdfe9752c1c4b106c8
GerardBurgues <gerard61999@gmail.com>
2021-01-21 07:43:44 +0100

    Changees in queries. Returning table and using less joins

--- src/main/resources/db/changelog/db.changelog-4.sql
+++ src/main/resources/db/changelog/db.changelog-4.sql
@@ -16,7 +16,9 @@ ALTER TABLE stats
     add column visitor_team_id    bigint  NOT NULL,
     add column winner_team_id     bigint  NOT NULL;
 
-
+ALTER TABLE stats
+    add column home_team_name    text,
+    add column visitor_team_name text;
 
 CREATE OR REPLACE FUNCTION
     best_player(player_id BIGINT, first_name TEXT, last_name TEXT, position TEXT, points DOUBLE PRECISION)
@@ -48,22 +50,33 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--Players who play more than 20 ‘(Average) for center position, sorted(percentage of freethrow)G
 CREATE OR REPLACE FUNCTION
-    center_player(minutes TEXT, Player_ID BIGINT, first_name TEXT, last_name TEXT, pos TEXT)
-    RETURNS TEXT AS
+    center_player()
+    RETURNS table
+            (
+                minutes               text,
+                first_name            text,
+                last_name             text,
+                position              text,
+                free_throw_percentage double precision
+            )
+    language plpgsql
+as
 $$
 BEGIN
-    SELECT averages.minutes,
-           p.first_name,
-           p.last_name,
-           p.position == 'C',
-           averages.free_throw_percentage
-    FROM averages
-             JOIN players p ON p.id = averages.player_id
-    WHERE averages.minutes < ('20:00')
-      AND position = 'C'
-    ORDER BY averages.free_throw_percentage;
-    RETURN first_name;-- here may change
+    return query
+        SELECT averages.minutes,
+               p.first_name,
+               p.last_name,
+               p.position,
+               averages.free_throw_percentage
+        FROM averages
+                 JOIN players p ON p.id = averages.player_id
+        WHERE averages.minutes < ('20:00')
+          AND position = 'C'
+        ORDER BY averages.free_throw_percentage;
+
 END;
 $$ LANGUAGE plpgsql;
 
@@ -84,18 +97,27 @@ $$ LANGUAGE plpgsql;
 
 -- Sorting by division show player name  with more than 15 points and 7  average assist G
 CREATE OR REPLACE FUNCTION
-    sort_by_division(division TEXT, points DOUBLE PRECISION, assist DOUBLE PRECISION)
-    RETURNS TEXT AS
+    sort_by_division()
+    RETURNS table
+            (
+                points     double precision,
+                assists    double precision,
+                first_name text,
+                last_name  text,
+                division   text
+            )
+    language plpgsql
+as
 $$
 BEGIN
-    SELECT averages.points, averages.assists, p.first_name, p.last_name, t.division
-    FROM averages
-             JOIN players p ON averages.player_id = p.id
-             JOIN teams t ON p.team_id = t.id
-    WHERE averages.points >= (15)
-      AND averages.assists >= (7)
-    ORDER BY t.division;
-    RETURN points; --here may change
+    return query
+        SELECT averages.points, averages.assists, p.first_name, p.last_name, t.division
+        FROM averages
+                 JOIN players p ON averages.player_id = p.id
+                 JOIN teams t ON p.team_id = t.id
+        WHERE averages.points >= (15)
+          AND averages.assists >= (7)
+        ORDER BY t.division;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -118,65 +140,84 @@ $$ LANGUAGE plpgsql;
 
 -- Show the games and players in which players has at least 10 assists, 10 points and has won a match (player were in winning team)G
 CREATE OR REPLACE FUNCTION
-    show_specific_players(p_points DOUBLE PRECISION, p_assist DOUBLE PRECISION)
-    RETURNS TEXT AS
+    show_specific_players()
+    RETURNS table
+            (
+                game_id            bigint,
+                points             double precision,
+                assists            double precision,
+                season             integer,
+                home_team_score    integer,
+                visitor_team_score integer,
+                home_team_name     text,
+                visitor_team_name  text,
+                winner_team_id     bigint,
+                home_team_id       bigint,
+                visitor_team_id    bigint
+            )
+    language plpgsql
+as
 $$
 DECLARE
     -- does not have conflict with parameters
-    v_winner BOOL;
+    v_winner BOOL; --1 has won the match, 0 hasn't won the match
 
 BEGIN
-    SELECT stats.game_id,
-           stats.points,
-           stats.assists,
-           g.season,
-           g.home_team_score,
-           g.visitor_team_score,
-           t.name,
-           t2.name,
-
-           CASE
-               WHEN stats.team_id = g.visitor_team_score
-                   THEN (CASE
-                             WHEN g.visitor_team_score > g.home_team_score
-                                 THEN winner = 0 -- player has won
-                             ELSE winner = 1
-                   END)
-               WHEN stats.team_id = g.home_team_score
-                   THEN CASE
-                            WHEN g.visitor_team_score > g.home_team_score
-                                THEN winner = 0 -- player has won
-                            ELSE winner = 1
-                   END
-               END AS winner
-    FROM stats
-             JOIN games g ON stats.game_id = g.id
-             JOIN teams t ON g.visitor_team_id = t.id
-             JOIN teams t2 ON g.home_team_id = t2.id
-    WHERE stats.points >= 10
-      AND stats.assists >= 10
-      AND winner = 0;
-
+    return query
+        SELECT stats.game_id,
+               stats.points,
+               stats.assists,
+               stats.season,
+               stats.home_team_score,
+               stats.visitor_team_score,
+               stats.home_team_name,
+               stats.visitor_team_name,
+               stats.winner_team_id,
+               stats.home_team_id,
+               stats.visitor_team_id,
+               CASE
+                   WHEN stats.team_id == stats.winner_team_id
+                       THEN v_winner = 1
+                   WHEN stats.team_id != stats.winner_team_id
+                       THEN v_winner = 0
+                   END v_winner
+        FROM stats
+        WHERE stats.points >= 10
+          AND stats.assists >= 10
+          AND v_winner = 1
+        ORDER BY stats.points;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Compare how many minutes and threepointers attempted per game have done the top 10 players with more than 38% three pointer average. G
 
 CREATE OR REPLACE FUNCTION
-    show_best_3_pt(points DOUBLE PRECISION, percentage_3 DOUBLE PRECISION)
-    RETURNS TEXT AS
+    show_best_3_pt()
+    RETURNS table
+            (
+                minutes                  text,
+                three_pointer_percentage double precision,
+                firs_name                text,
+                last_name                text,
+                team_name                text,
+                three_pointer_attempted  integer,
+                three_pointer_made       integer
+            )
+    language plpgsql
+as
 $$
 BEGIN
-    SELECT averages.minutes,
-           averages.three_pointer_percentage,
-           p.first_name,
-           p.last_name,
-           t.name,
-           s.three_pointers_attempted,
-           s.three_pointers_made
-    FROM averages
-             JOIN players p ON averages.player_id = p.id
-             JOIN teams t ON p.team_id = t.id
+    return query
+        SELECT averages.minutes,
+               averages.three_pointer_percentage,
+               p.first_name,
+               p.last_name,
+               t.name,
+               s.three_pointers_attempted,
+               s.three_pointers_made
+        FROM averages
+                 JOIN players p ON averages.player_id = p.id
+                 JOIN teams t ON p.team_id = t.id
              JOIN stats s ON p.id = s.player_id
     WHERE averages.three_pointer_percentage >= 38
     ORDER BY averages.three_pointer_percentage
