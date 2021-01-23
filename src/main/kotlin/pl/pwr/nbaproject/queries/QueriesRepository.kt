50f1f602a7f05fe08996c6cfc115ee328461fbf0
Michał Regulski <regulskimichal@outlook.com>
2021-01-23 16:33:59 +0100

    QueriesRepository initial version

---
+++ src/main/kotlin/pl/pwr/nbaproject/queries/QueriesRepository.kt
@@ -0,0 +1,21 @@
+package pl.pwr.nbaproject.queries
+
+import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitOne
+import org.springframework.stereotype.Component
+
+@Component
+class QueriesRepository(
+    val databaseClient: DatabaseClient,
+) {
+
+    suspend fun findWinnerId(gameId: Long): Long {
+        val result = databaseClient.sql("SELECT find_winner_id(:gameId)")
+            .bind("gameId", gameId)
+            .fetch()
+            .awaitOne()
+
+        return result["find_winner_id"] as Long
+    }
+
+}
